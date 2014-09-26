a1128064aba6b8583982c7352fb9971de7c901a9
Nikos Gereoudakis <gereoudakis@gmail.com>
2014-09-26 17:19:37 +0300

    fix plugin routes

--- templates/plugin/server/src/config/routes.js
+++ templates/plugin/server/src/config/routes.js
@@ -1,7 +1,6 @@
 'use strict';
-var router = require('express').Router;
-
 var sample = require('./../controllers/sample_controller');
-router.get('/sample', sample.index);
 
-module.exports = router;
+module.exports = function (router) {
+    router.get('/sample', sample.index);
+};

7dc281d3d1004c64e942e65ae8e54d83ebc0604c
Nikos Gereoudakis <gereoudakis@gmail.com>
2014-09-26 17:06:22 +0300

    fix package.json path

--- templates/app/server/src/index.js
+++ templates/app/server/src/index.js
@@ -2,4 +2,4 @@ var path = require('path');
 var installable = require('installable');
 var app = require('./app');
 
-installable.startServer(app, path.resolve('package.json'));
+installable.startServer(app, path.resolve(__dirname + './../../package.json'));

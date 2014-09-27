6051beb3e58b686761fe2730fa2bbd16b7141083
Nikos Gereoudakis <gereoudakis@gmail.com>
2014-09-27 13:33:50 +0300

    adds a comment for setting pluginsPath

--- templates/app/server/src/index.js
+++ templates/app/server/src/index.js
@@ -2,4 +2,8 @@ var path = require('path');
 var installable = require('installable');
 var app = require('./app');
 
+// set pluninsPath, defaults to '/plugins'
+// thus a plugin named 'test' will expose its routes
+// under '/plugins/test/*'
+// app.set('pluginsPath', 'myPluginsPath');
 installable.startServer(app, path.resolve(__dirname + './../../package.json'));
